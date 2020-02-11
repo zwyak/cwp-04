105f0ddbbe68809d995b095833f5a976392f18a9
zwyak <vlad.senyuk97@gmail.com>
2020-02-11 15:08:53 +0300

    Updated project files

--- client.js
+++ client.js
@@ -1,6 +1,10 @@
 // client.js
 const net = require('net');
 const port = 8124;
+const firstRequestStr = 'REMOTE';
+const successReq = 'ASC';
+const failedReq = 'DEC';
+
 
 const client = new net.Socket();
 
@@ -8,12 +12,21 @@ client.setEncoding('utf8');
 
 client.connect(port, function() {
   console.log('Connected');
-  client.write('\r\nHello, Server!\r\nLove,\r\nClient.\r\n');
+  client.RequestNumber = 0;
+  client.write(firstRequestStr);
 });
 
 client.on('data', function(data) {
-  console.log(data);
-  client.destroy();
+  client.RequestNumber = client.RequestNumber + 1;
+  if ( (data == successReq) && (client.RequestNumber == 1) ){
+
+    }
+  }else if ( (data != successReq) && (client.RequestNumber == 1) ){
+    console.log(data);
+    client.destroy();
+  }else{
+    
+  }
 });
 
 client.on('close', function() {
--- server.js
+++ server.js
@@ -2,14 +2,27 @@
 const net = require('net');
 const port = 8124;
 
+let seed = 3106;
+
 const server = net.createServer((client) => {
   console.log('Client connected');
 
   client.setEncoding('utf8');
+  client.ID = Date.now() + seed++;
+  client.RequestNumber = 0;
 
   client.on('data', (data) => {
-    console.log(data);
-    client.write('\r\nHello!\r\nRegards,\r\nServer\r\n');
+    client.RequestNumber = client.RequestNumber + 1;
+    if ( (data == firstRequestStr) && (client.RequestNumber == 1) ){
+      console.log(data);
+      client.write(successReq);
+    }else if ( (data != firstRequestStr) && (client.RequestNumber == 1) ){
+      console.log(data);
+      client.write(failedReq);
+      client.destroy();
+    }else{
+      console.log(data);
+    }
   });
 
   client.on('end', () => console.log('Client disconnected'));
